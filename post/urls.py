26b19628b47f4c4c9865e884ef080f945b5c1066
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-19 00:04:36 +0330

    create a list of post and ability to add post

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AllPostView
+from .views import AllPostView,PostCreateView
 
 urlpatterns = [
-    path('',AllPostView.as_view(),name='home')
+    path('',AllPostView.as_view(),name='home'),
+    path('new_post/',PostCreateView.as_view(),name='new_post'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,4 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from .models import Post
 
-class AllPostView(TemplateView):
+class AllPostView(ListView):
+    model=Post
     template_name = 'home.html'
+
+class PostCreateView(CreateView):
+    model=Post
+    template_name = 'new_post.html'
+    fields=['author','title','text',]
