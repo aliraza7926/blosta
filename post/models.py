43dbd74293b26e13de23c807084f8bce7dfaffca
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-18 23:37:14 +0330

    creat post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    text=models.TextField()
+    author=models.CharField(max_length=40)
+    title=models.CharField(max_length=250)
