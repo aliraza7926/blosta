1106723e18b619ac35e2d5833e65e8524c62f23a
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-19 00:26:25 +0330

    create review model

--- post/models.py
+++ post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+
 class Post(models.Model):
     text=models.TextField()
     author=models.CharField(max_length=40)
@@ -7,3 +8,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+class Review(models.Model):
+    author=models.CharField(max_length=40)
+    review=models.CharField(max_length=250)
