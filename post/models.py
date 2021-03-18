71674d7af2e62a167429a476e09a810924f412c9
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-19 00:01:19 +0330

    add get_absolute_url

--- post/models.py
+++ post/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
+from django.urls import reverse
 class Post(models.Model):
     text=models.TextField()
     author=models.CharField(max_length=40)
     title=models.CharField(max_length=250)
+
+    def get_absolute_url(self):
+        return reverse('home')
